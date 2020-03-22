ed20739f7512ae484243855a35e5c3da228c54f7
Palladium02 <seanmeyer544@gmail.com>
2020-03-22 15:19:35 +0100

    rewriten the whole thing

---
+++ src/photon.js
@@ -0,0 +1,80 @@
+function isObject(o) {
+    return typeof(o) === 'object' ? true : false;
+}
+
+function isArray(a) {
+    return Array.isArray(a);
+}
+
+class Photon {
+    constructor(options) {
+        if(isObject(options)) {
+            this.root = document.querySelector(options.el);
+            this.data = options.data;
+            this.lifecycle;
+            this.DIRECTIVEKEYS = [
+                'p-if',
+                'p-else',
+                'p-for'
+            ];
+            this.directiveELements = {
+                'p-if': [],
+                'p-else': [],
+                'p-for': []
+            }
+            this.traverseRoot();
+            this.lifeCycle();
+        } else {
+            console.error('');
+            return undefined;
+        }
+    }
+
+    traverseRoot() {
+        this.DIRECTIVEKEYS.forEach(KEY => {
+            let matches = this.root.querySelectorAll(`[${KEY}]`);
+            matches.forEach(match => {
+                this.directiveELements[KEY].push([match, match.getAttribute(KEY), match.parentNode]);
+                match.removeAttribute(KEY);
+            });
+
+        });
+    }
+
+    lifeCycle() {
+        this.traverseRoot();
+        this.lifecycle = setInterval(() => {
+            for(let [key, value] of Object.entries(this.directiveELements)) {
+                value.forEach(value => {
+                    switch (key) {
+                        case 'p-if':
+                            eval(value[1]) ? value[0].style.display = 'initial' : value[0].style.display = 'none';
+                            break;
+                        case 'p-for':
+                            let parent = value[0].parentNode;
+                            if(isArray(this.data[value[1]])) {
+                                parent = value[2];
+                                if(parent.children.length != this.data[value[1]].length) {
+                                    parent.innerHTML = '';
+                                    this.data[value[1]].forEach(dataElement => {
+                                        let loopedChild = value[0].cloneNode(true);
+                                        loopedChild.textContent = dataElement;
+                                        parent.appendChild(loopedChild);
+                                    });
+                                }
+                            } else {
+                                console.error('p-for expects typeof Array');
+                                this.killLifeCycle();
+                            }
+                        default:
+                            break;
+                    }
+                });
+            }
+        }, 0);
+    }
+
+    killLifeCycle() {
+        clearInterval(this.lifecycle);
+    }
+}
